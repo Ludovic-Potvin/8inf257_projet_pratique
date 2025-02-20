96192db260b94f31d9a81d4549e2b2b5a2d88112
MarionM64 <marion_marty4@orange.fr>
2025-02-20 18:20:31 -0500

    changement des couleurs pour les priorités

--- project/app/src/main/java/com/project/mobile/presentation/components/StoryCard.kt
+++ project/app/src/main/java/com/project/mobile/presentation/components/StoryCard.kt
@@ -10,12 +10,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,10 +26,10 @@ import androidx.compose.ui.unit.sp
 import androidx.media3.common.util.Log
 import androidx.media3.common.util.UnstableApi
 import androidx.navigation.NavController
-import com.project.mobile.navigation.Screen
 import com.project.mobile.presentation.Priorite
 import com.project.mobile.presentation.StoryVM
-import com.project.mobile.ui.theme.DarkPurple
+import com.project.mobile.ui.theme.LightPurple
+import com.project.mobile.ui.theme.MediumPurple
 import com.project.mobile.ui.theme.Purple
 import com.project.mobile.ui.theme.WhitePurple
 import suezOneRegular
@@ -45,9 +42,9 @@ import java.time.format.DateTimeFormatter
 fun StoryCard(story: StoryVM, navController: NavController){
     val routineId = story.id
     val background = when (story.priorite) {
-        Priorite.BASSE -> WhitePurple
+        Priorite.BASSE -> LightPurple
         Priorite.MOYENNE -> Purple
-        Priorite.ELEVEE -> DarkPurple
+        Priorite.ELEVEE -> MediumPurple
     }
     Log.d("routineId", "routineId=$routineId")
     Box(
--- project/app/src/main/java/com/project/mobile/ui/theme/Color.kt
+++ project/app/src/main/java/com/project/mobile/ui/theme/Color.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.graphics.Color
 
 
 val Purple = Color.hsl(291F, 0.54F, 0.45F, 0.55F)
+val MediumPurple = Color.hsl(291F, 0.54F, 0.45F, 0.70F)
+val LightPurple = Color.hsl(291F, 0.40F, 0.70F, 0.7F)
 val DarkPurple = Color.hsl(291F, 0.87F, 0.30F, 0.70F)
 val WhitePurple = Color.hsl(291F, 0.30F, 0.87F, 0.7F)
 
