f5bad352028a09dc2df2d311492adfa5a34ae552
Ludovic Potvin <ludovicpotvin@hotmail.com>
2025-04-11 18:02:41 -0400

    Settings

--- project/app/src/main/java/com/project/mobile/presentation/preference/PreferenceScreen.kt
+++ project/app/src/main/java/com/project/mobile/presentation/preference/PreferenceScreen.kt
@@ -96,7 +96,7 @@ fun PreferenceForm(navController: NavController, viewModel: ThemeViewModel) {
                     )
                 }
                 Text(
-                    "Parameters",
+                    "Settings",
                     color = Color.White,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold,
