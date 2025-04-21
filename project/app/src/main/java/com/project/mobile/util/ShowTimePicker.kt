91f09a70d6f80b01d4134bcdecdc6802b932bb2f
Rithan04 <julirithan29@gmail.com>
2025-04-21 19:02:03 -0400

    Time Picker normalement fonctionnel

--- project/app/src/main/java/com/project/mobile/util/ShowTimePicker.kt
+++ project/app/src/main/java/com/project/mobile/util/ShowTimePicker.kt
@@ -1,12 +1,25 @@
 package com.project.mobile.util
 
+import android.app.Activity
 import android.app.TimePickerDialog
 import android.content.Context
+import android.content.ContextWrapper
 import java.time.LocalTime
 
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else -> null
+}
 fun showTimePicker(context: Context, initialTime: LocalTime, onTimeSelected: (LocalTime) -> Unit) {
+    val activity = context.findActivity()
+    if (activity == null || activity.isFinishing) {
+        println("Activity context is not valid")
+        return
+    }
+
     val timePicker = TimePickerDialog(
-        context,
+        activity,
         { _, selectedHour, selectedMinute ->
             val newTime = LocalTime.of(selectedHour, selectedMinute).withSecond(0).withNano(0)
             onTimeSelected(newTime)
